9102dbf3b9dbf4c2985c149857188c5263c9f821
Mamadou <125818224+mamadou186@users.noreply.github.com>
2024-01-03 08:40:43 -0500

    Add files via upload

---
+++ Fichiers_C/new_game.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#include "fonctions.c"
+
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        fprintf(stderr, "Usage: %s model_file index_file word1 word2 ...\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    const char *modelFile = argv[1];
+    const char *indexFile = argv[2];
+    int numWords = argc - 3;
+    char **words = argv + 3;
+
+    new_game(modelFile, indexFile, numWords, words);
+
+    return 0;
+}
