823d8a74169c92bc4f7bb3aa611654378b1fb0c3
BA MAMADOU <mamadou.ba2@heckel02.u-pem.fr>
2024-01-18 17:49:48 +0100

    Vincent Redirection.php

--- redirection.php
+++ redirection.php
@@ -1,13 +1,10 @@
 <?php
 session_start(); // Démarre une nouvelle session ou reprend une session existante
 
-// Vérifie si une certaine variable de session est définie et non nulle
-
-if (!isset($_SESSION['pseudo'] && isset(($_SESSION['mdp']))) {
-    
-    // Si la session n'est pas reconnue, alors on est redirigé vers la page 'index.php'
+// Vérifie si certaines variables de session sont définies et non nulles
+if (!isset($_SESSION['pseudo']) && !isset($_SESSION['mdp'])) {
+    // Si une des sessions n'est pas reconnue, alors on est redirigé vers la page 'index.php'
     header("Location: index.php");
-    exit(); 
+    exit();
 }
 ?>
-
