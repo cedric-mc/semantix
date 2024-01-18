11478baad7265daf0f85a7b1cb9a6125b32c17cf
Maloum Elyas <elyasmaloum@gmail.com>
2024-01-18 19:01:49 +0100

    java classes

---
+++ Fichiers_Java/SAE/src/main/java/projet/tree/Tree.java
@@ -0,0 +1,30 @@
+package projet.tree;
+
+import projet.branch.Branch;
+
+import java.util.ArrayList;
+
+public class Tree {
+    private final ArrayList<Branch> branches;
+
+    public Tree(ArrayList<Branch> branches) {
+        this.branches = branches;
+    }
+
+    public ArrayList<Branch> getBranches() {
+        return branches;
+    }
+
+    public void addBranch(Branch branch) {
+        branches.add(branch);
+    }
+
+    public ArrayList<Branch> detectAllCycles(){
+        ArrayList<Branch> cycle = new ArrayList<Branch>();
+        return null;
+    }
+
+    public void deleteWeakestBranchFromCycle() {
+
+    }
+}
