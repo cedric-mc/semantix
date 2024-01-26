729465c30d4537d54e66c78fb403f9fffd5f2b84
elyasmlm <117287234+elyasmlm@users.noreply.github.com>
2024-01-26 14:42:53 +0100

    Update regles.php
    règles

--- regles.php
+++ regles.php
@@ -11,14 +11,32 @@
 
 <div class="container">
     <div class="box">
-        <h1>Règles</h1>
+        <h1>Comment jouer à Semonkey</h1>
         <br> <br>
         <p>
-            Dans le jeu Semonkey vous débutez avec deux mots, un mot d'arrivée et un mot de départ.
-
-
-
-
+            Pour jouer à Semonkey, rien de plus simple ! Suivez les règles du jeu. 
+        </p>
+         <br> <br>
+        <h2>Découlement du jeu :</h2>
+        <p>   
+            
+            Le jeu commence avec deux mots affichés : le mot de départ et le mot d'arrivée.
+            Ajoutez un mot en l'écrivant et en appuyant sur le bouton d'ajout. 
+            Une fois le mot ajouté, un lien a été crée avec les autres. 
+            Attention, vous n'avez que 3 mots d'entréé avant que la partie ne commence, mais si vous êtes satisfait de votre score avant vous pouvez décider de l'arrêter.
+        </p>
+        <br>
+        <h2>Scoring :</h2>
+        <p>   
+            Chaque lien entre deux mot est calculé par rapport à leur similarité qu'elle soit orthographique ou lexicographique, choisissez bien vos mots !
+            Le score final sera le score de la branche la plus faible liant le mot de départ et le mot d'arrivée divisé le nombre de mots entrés. 
+            Attention à ne pas faire un score trop faible dès le début !
+        </p>
+        <br>
+        <h2>Restriction :</h2>
+        <p>   
+            N'ajoutez pas deux fois le même mot !
+            Faites attention aux mots que vous utilisez, il ne doivent blesser ou porter préjudice à aucune minoroté quelle qu'elle soit.
         </p>
     </div>
 </div>
