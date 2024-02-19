82b92133eea8d8d02a3a0d28ce08c56547774764
cedric-mc <cedric.mc11@gmail.com>
2024-02-19 09:18:04 +0100

    19/02 09:17

--- game/start_game.php
+++ game/start_game.php
@@ -4,12 +4,16 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 $_SESSION['pseudo'] = "cedric-mc";
-$verif_mot = -1;
-do { // Tant que les mots de départ et de cible n'ont pas une similarité inférieure à 10.
+// Erreurs PHP
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+do {
+    $verif_mot = -1;
     while ($verif_mot == -1) {
         $mot1 = randomWord('Liste_mots.txt');
-        $commande_verif_mot1 = './C/bin/dictionary_lookup C/arbre_lexicographique.lex ' . $mot1;
-        $verif_mot1 = shell_exec($commande_verif_mot1);
+        $commande_verif = "./C/bin/dictionary_lookup C/arbre_lexicographique.lex $mot1";
+        $verif_mot = shell_exec($commande_verif);
     }
     $verif_mot = -1;
 
@@ -18,27 +22,12 @@ do { // Tant que les mots de départ et de cible n'ont pas une similarité infé
         while ($mot1 == $mot2) {
             $mot2 = randomWord('Liste_mots.txt');
         }
-        $commande_verif_mot2 = './C/bin/dictionary_lookup C/arbre_lexicographique.lex ' . $mot2;
-        $verif_mot2 = shell_exec($commande_verif_mot2);
+        $commande_verif = "./C/bin/dictionary_lookup C/arbre_lexicographique.lex $mot2";
+        $verif_mot = shell_exec($commande_verif);
     }
-    $verif_mot = -1;
-    $distance = 100;
-    $fichier = fopen("partie/game_data_$_SESSION[pseudo].txt", "r");
-    // Lire le fichier jusqu’à la 8ème ligne et stocker ce qui se trouve après "distance: " dans $distance
-    for ($i = 0; $i < 8; $i++) {
-        $ligne = fgets($fichier);
-        if ($i == 7) {
-            $chaine = $ligne;
-            $parties = explode("distance:", $chaine);
-            if (count($parties) > 1) {
-                $distance = floatval(trim($parties[1]));
-            }
-            $distance = substr($ligne, strpos($ligne, ", distance: ") + 12);
-        }
-    }
-} while ($distance > 10);
+} while (1 == 0);
 
-$commande_start_game = './C/bin/new_game C/fasttext-fr.bin ' . $mot1 . ' ' . $mot2 . ' ' . $_SESSION['pseudo'];
+$commande_start_game = "./C/bin/new_game C/fasttext-fr.bin $mot1 $mot2 $_SESSION[pseudo]";
 exec($commande_start_game);
 
 $commandeJava = "/home/3binf2/mariyaconsta02/jdk-21/bin/java -cp ChainMotor/target/classes fr.uge.main.Main partie/game_data_$_SESSION[pseudo].txt partie/mst_$_SESSION[pseudo].txt partie/best_path_$_SESSION[pseudo].txt 2>&1";
