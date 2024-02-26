f218a45868e54744799ffb69e8c94eb0cbea2a31
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-02-26 11:36:54 +0100

    Ajouter une condition pour afficher une alerte d'erreur en JavaScript

--- game/game.php
+++ game/game.php
@@ -101,11 +101,12 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
         <script>
             document.addEventListener('DOMContentLoaded', function() {
-                if (isset($_GET['erreur'])) {
-                    window.alert('Erreur : ' + "<?php echo $messagesErreur[$_GET['erreur']]; ?>");
-                }
+                <?php if (isset($_GET['erreur'])): ?>
+                    window.alert('Erreur : <?php echo $messagesErreur[$_GET['erreur']]; ?>');
+                <?php endif; ?>
             });
-
+        </script>
+        <script>
             // Fonction pour charger les données depuis PHP
             function loadData() {
                 // Charger les données depuis le fichier PHP
