503cace6e5a0ae80264bb99029fc896f02be9d0f
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-03-08 23:01:11 +0100

    Supprimer les modals inutilisés et inclure les modifications.php

---
+++ profil/changes.php
@@ -0,0 +1,39 @@
+<div class="modal fade" id="emailModal" aria-labelledby="emailModalLabel" tabindex="-1" style="display: none;" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="emailModalLabel">Modal 1</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                Show a second modal and hide this one with the button below.
+            </div>
+        </div>
+    </div>
+</div>
+<div class="modal fade" id="mdpModal" aria-labelledby="mdpModalLabel" tabindex="-1" style="display: none;" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="mdpModalLabel">Changer le mot de passe</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                rgthj,k;lokjhg
+            </div>
+        </div>
+    </div>
+</div>
+<div class="modal fade" id="photoModal" aria-labelledby="photoModalLabel" tabindex="-1" style="display: none;" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="photoModalLabel">Modal 3</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                And lastly, the third one. This is just a simple modal, nothing more.
+            </div>
+        </div>
+    </div>
+</div>
--- profil/index.php
+++ profil/index.php
@@ -149,65 +149,7 @@
                 </div>
             </div>
         </main>
-        <!-- <div id="modal-mdp" class="modal fade">
-            <div class="modal-content">
-                <span class="btn-close">&times;</span>
-                <form action="script-password.php" method="POST">
-                    <div class="input-field">
-                        <input type="password" id="password1" name="password1" required>
-                        <label for="password1">Ancien mot de passe :</label>
-                    </div>
-                    <div class="input-field">
-                        <input type="password" id="password2" name="password2" required>
-                        <label for="password2">Nouveau mot de passe :</label>
-                    </div>
-                    <div class="input-field">
-                        <input type="password" id="password3" name="password3" required>
-                        <label for="password3">Confirmer le nouveau mot de passe :</label>
-                    </div>
-                    <button id="formButton" type="submit" class="btn fw-semibold">Valider</button>
-                </form>
-            </div>
-        </div> -->
-        <div class="modal fade" id="emailModal" aria-labelledby="emailModalLabel" tabindex="-1" style="display: none;" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="emailModalLabel">Modal 1</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        Show a second modal and hide this one with the button below.
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="modal fade" id="mdpModal" aria-labelledby="mdpModalLabel" tabindex="-1" style="display: none;" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="mdpModalLabel">Changer le mot de passe</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        rgthj,k;lokjhg
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="modal fade" id="photoModal" aria-labelledby="photoModalLabel" tabindex="-1" style="display: none;" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="photoModalLabel">Modal 3</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        And lastly, the third one. This is just a simple modal, nothing more.
-                    </div>
-                </div>
-            </div>
-        </div>
+        <?php include("changes.php"); ?>
         <script>
             $(document).ready(function() {
                 function updateElapsedTime() {
