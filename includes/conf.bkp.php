e0383c598dc654902c4c27155e9e82c6ec7f472f
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-03-08 21:15:18 +0100

    Mise à jour des fichiers de configuration et de connexion

--- includes/conf.bkp.php
+++ includes/conf.bkp.php
@@ -1,11 +1,23 @@
 <?php
-include("conf.php");
+// Base de données
+$user = "";
+$pass = "";
+$dbname = "";
+$server = "";
 
 try {
-    $cnx = new PDO("mysql:host=sqletud.u-pem.fr;dbname=$dbname;charset=utf8mb4", $user, $pass);
+    $cnx = new PDO("mysql:host=$server;dbname=$dbname;charset=utf8mb4", $user, $pass);
+    if ($cnx->getAttribute(PDO::ATTR_CONNECTION_STATUS)) {
+        echo "Connexion réussie";
+    }
 }
 catch (PDOException $e) {
     echo "ERREUR : La connexion a échouée";
 }
 
+// PHPMailer
+$username = "";
+$password = "";
+$host = "";
+$name = "";
 ?>
