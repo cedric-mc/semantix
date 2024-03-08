05fbc69bf2a16185fb118be5f5baa444fe043fad
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-03-08 15:32:41 +0100

    Supprimer les liens Bootstrap inutilisés dans change_password.php

--- profil/change_password.php
+++ profil/change_password.php
@@ -7,8 +7,6 @@
         <meta charset="UTF-8">
         <title>Changer Mot de passe - Semonkey</title>
         <link rel="stylesheet" href="../style/form.css">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
         <style>
             h1.title, label {
                 color: black;
