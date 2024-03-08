e1ecaa59ad1b6ed6da293f4562e35d686734c192
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-03-08 22:58:46 +0100

    Ajout d'une fenêtre modale pour les photos dans la page de profil

--- profil/index.php
+++ profil/index.php
@@ -195,6 +195,19 @@
                 </div>
             </div>
         </div>
+        <div class="modal fade" id="photoModal" aria-labelledby="photoModalLabel" tabindex="-1" style="display: none;" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="photoModalLabel">Modal 3</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        And lastly, the third one. This is just a simple modal, nothing more.
+                    </div>
+                </div>
+            </div>
+        </div>
         <script>
             $(document).ready(function() {
                 function updateElapsedTime() {
