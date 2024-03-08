356cde26cad429038a8f50a9c3d6085c307bc64a
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-03-08 15:38:58 +0100

    Suppression du lien vers le fichier form.css dans index.php

--- profil/index.php
+++ profil/index.php
@@ -54,7 +54,6 @@
         <meta charset="utf-8">
         <link rel="stylesheet" href="../style/style.css">
         <link rel="stylesheet" href="../style/css_profil.css">
-        <link rel="stylesheet" href="../style/form.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
         <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
