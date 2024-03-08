e30c7faf34a4dd093f2a635331223b62450bde36
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-03-08 19:48:49 +0100

    Supprimer le deuxième modal inutile dans le fichier index.php

--- profil/index.php
+++ profil/index.php
@@ -194,19 +194,6 @@
                 </div>
             </div>
         </main>
-        <div class="modal fade" id="modal-mdp" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
-            <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="exampleModalToggleLabel2">Modal 2</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        Hide this modal and show the first with the button below.
-                    </div>
-                </div>
-            </div>
-        </div>
         <!-- <div id="modal-mdp" class="modal fade">
             <div class="modal-content">
                 <span class="btn-close">&times;</span>
