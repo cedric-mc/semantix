3a9454a7d2dbf2bed20f5efa9693c4543f4f12f5
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-03-08 22:52:11 +0100

    Supprimer le code de connexion réussie dans conf.bkp.php

--- includes/conf.bkp.php
+++ includes/conf.bkp.php
@@ -7,9 +7,6 @@ $server = "";
 
 try {
     $cnx = new PDO("mysql:host=$server;dbname=$dbname;charset=utf8mb4", $user, $pass);
-    if ($cnx->getAttribute(PDO::ATTR_CONNECTION_STATUS)) {
-        echo "Connexion réussie";
-    }
 }
 catch (PDOException $e) {
     echo "ERREUR : La connexion a échouée";
