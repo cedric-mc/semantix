b698a9c27ecabd712ab4c425e8ef60cd65917527
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-03-08 23:02:01 +0100

    Modifier le titre du modal et ajouter un formulaire pour changer l'adresse email

--- profil/changes.php
+++ profil/changes.php
@@ -2,11 +2,21 @@
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="emailModalLabel">Modal 1</h1>
+                <h1 class="modal-title fs-5" id="emailModalLabel">Changer l'adresse email</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                Show a second modal and hide this one with the button below.
+                <form action="script-email.php" method="POST">
+                    <div class="input-field">
+                        <input type="email" name="email1" id="email1" required>
+                        <label for="email1">Ancienne adresse email :</label>
+                    </div>
+                    <div class="input-field">
+                        <input type="email" name="email2" id="email2" required>
+                        <label for="email2">Nouvelle adresse email :</label>
+                    </div>
+                    <button id="formButton" type="submit" class="btn fw-semibold">Valider</button>
+                </form>
             </div>
         </div>
     </div>
