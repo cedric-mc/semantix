03ef8250007a98c21a60ca76b613c518e5edc7d4
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-03-09 08:53:23 +0100

    Mise à jour de la gestion des erreurs d'email dans changes.php

--- profil/changes.php
+++ profil/changes.php
@@ -26,33 +26,13 @@
                             <button id="formButton" type="submit" class="btn btn-outline-primary fw-semibold">Valider</button>
                         </form>
                     </div>
-                    <div class="modal-footer">
-                        <?php
-                            // Si le message d'erreur est différent de 0
-                            if ($codeEmail > 0 && $codeEmail < 6) {
-                                echo "<br><div id='msg-error' class='alert' role='alert'></div>";
-                            }
-                        ?>
+                    <div class="modal-footer text-center">
                         <?php if ($codeEmail > 0 && $codeEmail < 6) { ?>
-                            <script>
-                                // Je récupère le message d’erreur
-                                let msgCode = <?php echo json_encode($codeEmail); ?>;
-                                let msgError = <?php echo json_encode($erreursEmail[$codeEmail][0]); ?>;
-                                // Si le message d’erreur est différent de 0
-                                if (msgCode > 0 && msgCode < 6) {
-                                    // J'affiche le message d'erreur
-                                    document.getElementById('msg-error').innerHTML = msgError;
-                                    // Je change la couleur du message d'erreur
-                                    document.getElementById('msg-error').classList.add(<?php echo json_encode($erreursEmail[$codeEmail][1]); ?>);
-                                    document.getElementById('msg-error').classList.add('visible');
-                                    // Après l'expiration du cookie, on actualise la page pour le supprimer
-                                    setTimeout(function () {
-                                        window.location.href = 'change_email.php';
-                                    }, 10000);
-                                }
-                            </script>
+                            <div id="msgEmail" class="alert <?php echo $erreursEmail[$codeEmail][1]; ?>" role="alert">
+                                <?php echo $erreursEmail[$codeEmail][0]; ?>
+                            </div>
                         <?php } ?>
-                    </div   >
+                    </div>
                 </div>
             </div>
         </div>
