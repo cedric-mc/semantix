d5003f5f71575c595ea2e73625b55d879895164b
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-03-09 08:46:31 +0100

    Ajouter la gestion des messages d'erreur pour le changement d'email

--- profil/changes.php
+++ profil/changes.php
@@ -33,6 +33,25 @@
                                 echo "<br><div id='msg-error' class='alert' role='alert'></div>";
                             }
                         ?>
+                        <?php if ($codeEmail > 0 && $codeEmail < 6) { ?>
+                            <script>
+                                // Je récupère le message d’erreur
+                                let msgCode = <?php echo json_encode($codeEmail); ?>;
+                                let msgError = <?php echo json_encode($erreursEmail[$codeEmail][0]); ?>;
+                                // Si le message d’erreur est différent de 0
+                                if (msgCode > 0 && msgCode < 6) {
+                                    // J'affiche le message d'erreur
+                                    document.getElementById('msg-error').innerHTML = msgError;
+                                    // Je change la couleur du message d'erreur
+                                    document.getElementById('msg-error').classList.add(<?php echo json_encode($erreursEmail[$codeEmail][1]); ?>);
+                                    document.getElementById('msg-error').classList.add('visible');
+                                    // Après l'expiration du cookie, on actualise la page pour le supprimer
+                                    setTimeout(function () {
+                                        window.location.href = 'change_email.php';
+                                    }, 10000);
+                                }
+                            </script>
+                        <?php } ?>
                     </div>
                 </div>
             </div>
