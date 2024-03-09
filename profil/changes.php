2149b797d7ea60f678ea64c326a69a7372276289
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-03-09 08:32:33 +0100

    Changer le titre du modal pour "Changer la photo de profil"

--- profil/changes.php
+++ profil/changes.php
@@ -66,7 +66,7 @@
             <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="photoModalLabel">Modal 3</h1>
+                        <h1 class="modal-title fs-5" id="photoModalLabel">Changer la photo de profil</h1>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
