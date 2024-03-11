48202e779b5da1676fa3ee03ba59aaa0777ddaae
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-03-11 09:45:56 +0100

    Ajouter une feuille de style Bootstrap à la page d'accueil

--- amis/index.php
+++ amis/index.php
@@ -44,6 +44,7 @@
         <meta charset="UTF-8">
         <title>Amis - Semonkey</title>
         <link rel="shortcut icon" href="../img/monkeyapp.png">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="../style/style.css">
         <link rel="stylesheet" href="../style/css_amis.css">
         <?php include("../includes/head.php"); ?>
