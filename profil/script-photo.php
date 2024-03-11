6d41eba3628ede5de6feb785975a3827b5351cf7
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-03-11 11:39:21 +0100

    Ajouter la fonctionnalité de mise à jour de la photo de profil

--- profil/script-photo.php
+++ profil/script-photo.php
@@ -1,5 +1,6 @@
 <?php
 include_once("../class/User.php");
+include_once("../includes/conf.php");
 session_start();
 if (!isset($_SESSION["user"])) {
     header("Location: ./");
@@ -8,6 +9,10 @@ if (!isset($_SESSION["user"])) {
 $user = User::createUserFromUser(unserialize($_SESSION["user"]));
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_FILES["image"])) {
     $image_data = file_get_contents($_FILES["photo"]["tmp_name"]);
+
+    $stmt = $cnx->prepare("UPDATE sae_users SET photo = ? WHERE num_user = ?");
+    $stmt->execute(array($image_data, $user->getIdUser()));
+    $stmt->closeCursor();
 } else {
     header("Location: ./");
     exit();
