344893e4859c0b628216c77c73788255744a0a88
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-03-11 19:49:43 +0100

    Ajouter la requête pour refuser une demande d'ami

--- includes/requetes.php
+++ includes/requetes.php
@@ -17,6 +17,7 @@
     $canAddFriend = "SELECT u.* FROM sae_users u, sae_friendship f WHERE (u.num_user = f.user_id OR u.num_user = f.friend_id) AND f.statut = 0 AND u.num_user != 22 AND u.statut = 1";
     $addFriend = "INSERT INTO sae_friendship (user_id, friend_id, statut) VALUES (:num_user, :friend_id, 0);";
     $acceptFriend = "UPDATE sae_friendship SET statut = 1 WHERE user_id = :num_user AND friend_id = :friend_id;";
+    $refuseFriend = "DELETE FROM sae_friendship WHERE user_id = :num_user AND friend_id = :friend_id;";
     $deleteFriend = "DELETE FROM sae_friendship WHERE user_id = :num_user AND friend_id = :friend_id OR user_id = :friend_id AND friend_id = :num_user;";
     $listUsers = "SELECT * FROM sae_users WHERE num_user NOT IN (SELECT friend_id FROM sae_friendship WHERE user_id = :idUser) AND num_user <> :num_user ORDER BY num_user;";
     $listUsersPseudo = "SELECT num_user, pseudo FROM sae_users WHERE num_user NOT IN (SELECT friend_id FROM sae_friendship WHERE user_id = :idUser) AND num_user <> :num_user ORDER BY pseudo;";
