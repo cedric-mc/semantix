ac1de821d5e5c294d9187624d1dfa1c447592e5c
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-03-11 11:40:28 +0100

    Ajouter la gestion des erreurs PHP et terminer le script-photo.php

--- profil/script-photo.php
+++ profil/script-photo.php
@@ -2,6 +2,10 @@
 include_once("../class/User.php");
 include_once("../includes/conf.php");
 session_start();
+// Erreur PHP
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 if (!isset($_SESSION["user"])) {
     header("Location: ./");
     exit();
@@ -13,6 +17,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_FILES["image"])) {
     $stmt = $cnx->prepare("UPDATE sae_users SET photo = ? WHERE num_user = ?");
     $stmt->execute(array($image_data, $user->getIdUser()));
     $stmt->closeCursor();
+
+    exit();
 } else {
     header("Location: ./");
     exit();
