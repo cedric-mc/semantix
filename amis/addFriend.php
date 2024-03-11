d54eecbf4d437818d78f8b32f2b241c630dc2924
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-03-11 09:53:09 +0100

    Ajouter un ami à la liste d'amis

--- amis/addFriend.php
+++ amis/addFriend.php
@@ -4,4 +4,21 @@ if (!isset($_GET['idUser'])) {
     exit;
 }
 $idUser = $_GET['idUser'];
+include_once("../class/User.php");
+include_once("../includes/conf.php");
+include_once("../includes/requetes.php");
+session_start();
+if (!isset($_SESSION['user'])) {
+    header('Location: ./');
+    exit;
+}
+$user = User::createUserFromUser(unserialize($_SESSION['user']));
+$idUser = $user->getIdUser();
+
+// Requête SQL pour ajouter un ami
+$addFriendRequest = $cnx->prepare($addFriend);
+$addFriendRequest->bindParam(":num_user", $idUser, PDO::PARAM_INT);
+$addFriendRequest->bindParam(":friend_id", $idUser, PDO::PARAM_INT);
+$addFriendRequest->execute();
+$addFriendRequest->closeCursor();
 ?>
