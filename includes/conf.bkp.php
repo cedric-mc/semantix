d4dd803bc741f7d22a1f013dbfa30e0d4eb0e6b2
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-03-12 09:53:27 +0100

    Mise à jour des informations de connexion à la base de données et de l'URL du site

--- includes/conf.bkp.php
+++ includes/conf.bkp.php
@@ -1,27 +1,27 @@
 <?php
-// Base de données
-$user = "";
-$pass = "";
-$dbname = "";
-$server = "";
+    // Base de données
+    $user = "";
+    $pass = "";
+    $dbname = "";
+    $server = "";
 
-try {
-    $cnx = new PDO("mysql:host=$server;dbname=$dbname;charset=utf8mb4", $user, $pass);
-}
-catch (PDOException $e) {
-    echo "ERREUR : La connexion a échouée";
-}
+    try {
+        $cnx = new PDO("mysql:host=$server;dbname=$dbname;charset=utf8mb4", $user, $pass);
+    }
+    catch (PDOException $e) {
+        echo "ERREUR : La connexion a échouée";
+    }
 
-// PHPMailer
-$username = "";
-$password = "";
-$host = "";
-$name = "";
+    // PHPMailer
+    $username = "";
+    $password = "";
+    $host = "";
+    $name = "";
 
-// Lien général
-$lienGeneral = "";
-// Lien vers la page de confirmation d'inscription
-$lienInscription = $lienGeneral . "inscription/script-confirmermail.php";
-// Lien vers la page de réinitialisation du mot de passe
-$lienReinitialisation = $lienGeneral . "forgotpassword/reset_password.php";
+    // Lien général vers le site
+    $lienGeneral = "";
+    // Vers la page de confirmation d'inscription
+    $lienInscription = $lienGeneral . "inscription/script-confirmermail.php";
+    // Vers la page de réinitialisation du mot de passe
+    $lienReinitialisation = $lienGeneral . "forgotpassword/reset_password.php";
 ?>
