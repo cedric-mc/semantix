5bfe412858ae6be6470dfab5ae16221a1763df4d
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-03-12 09:09:14 +0100

    Ajout du modèle de courrier pour le changement d'email

---
+++ mail/forgotpassword.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>Mail pour changement d'email</title>
+        <style>
+            header {
+                background-color: #f3f3f3;
+                padding: 20px;
+                text-align: center;
+            }
+
+            footer {
+                background-color: #f3f3f3;
+                padding: 20px;
+                text-align: center;
+            }
+
+            img {
+                width: 6.25rem;
+                height: auto;
+                margin: 20px;
+            }
+
+            p {
+                font-size: 20px;
+                text-align: center;
+            }
+        </style>
+    </head>
+
+    <body>
+        <header>
+            <h1>Changement d'email</h1>
+            <img src="cid:mylogo" alt="Semantic Analogy Explorer" width="100px" height="100px">
+        </header>
+        <p>
+            Bonjour <?php echo $_SESSION['pseudo']; ?>, Votre adresse e-mail a été modifiée. Si vous n'êtes pas à l'origine de cette modification, veuillez contacter l'administrateur du site.
+            <br><br>
+            Cordialement,<br>
+            L'équipe de Semonkey.
+        </p>
+        <footer>
+            <p>© 2024 Semonkey. Tous droits réservés.</p>
+        </footer>
+    </body>
+</html>
