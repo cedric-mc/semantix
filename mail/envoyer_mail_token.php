f3a70f5bd6c8e1ef887090ca0447b09405d7e946
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-03-12 21:22:50 +0100

    Ajouter la fonctionnalité de confirmation d'inscription par email

--- mail/envoyer_mail_token.php
+++ mail/envoyer_mail_token.php
@@ -1,49 +1,53 @@
 <?php
-session_start();
+    session_start();
+    if (!isset($_SESSION['pseudo_temp'])) {
+        header('Location: ../');
+        exit;
+    }
+    $pseudo = $_SESSION['pseudo_temp'];
+    include("../includes/conf.php");
 
-if (!isset($_SESSION['pseudo_temp'])) {
-    header('Location: ../');
-    exit;
-}
+    //requête récuperer le num_user de l'utilisateur
+    $queryGetNum = "SELECT num_user FROM sae_users WHERE pseudo = :pseudo";
+    $stmtGetNum = $cnx->prepare($queryGetNum);
+    $stmtGetNum->bindParam(":pseudo", $pseudo, PDO::PARAM_STR);
+    $stmtGetNum->execute();
 
-include 'includes/conf.php';
+    // Récupère le résultat
+    $num_user = $stmtGetNum->fetchColumn();
 
-$pseudo = $_SESSION['pseudo_temp'];
+    // Génération d'un code de confirmation (peut être un jeton unique)
+    $code_confirmation = bin2hex(random_bytes(16));
 
-//requête récuperer le num_user de l'utilisateur
-$queryGetNum = "SELECT num_user FROM sae_users WHERE pseudo = :pseudo";
-$stmtGetNum = $cnx->prepare($queryGetNum);
-$stmtGetNum->bindParam(":pseudo", $pseudo, PDO::PARAM_STR);
-$stmtGetNum->execute();
+    // Enregistrement du code de confirmation dans la base de données
+    $query_insert_confirmation = "INSERT INTO sae_confirmation_codes (num_user, code) VALUES (:num_user, :code)";
+    $stmt_insert_confirmation = $cnx->prepare($query_insert_confirmation);
+    $stmt_insert_confirmation->bindParam(":num_user", $num_user, PDO::PARAM_INT);
+    $stmt_insert_confirmation->bindParam(":code", $code_confirmation);
+    $stmt_insert_confirmation->execute();
 
-// Récupère le résultat
-$num_user = $stmtGetNum->fetchColumn();
+    //requête récuperer le mail de l'utilisateur
+    $queryGetMail = "SELECT email FROM sae_users WHERE pseudo = :pseudo";
+    $stmtGetmail = $cnx->prepare($queryGetMail);
+    $stmtGetmail->bindParam(":pseudo", $pseudo, PDO::PARAM_STR);
+    $stmtGetmail->execute();
+    // Récupère le résultat
+    $email = $stmtGetmail->fetchColumn();
 
-// Génération d'un code de confirmation (peut être un jeton unique)
-$code_confirmation = bin2hex(random_bytes(16));
+    include("mailer.php");
 
-// Enregistrement du code de confirmation dans la base de données
-$query_insert_confirmation = "INSERT INTO sae_confirmation_codes (num_user, code) VALUES (:num_user, :code)";
-$stmt_insert_confirmation = $cnx->prepare($query_insert_confirmation);
-$stmt_insert_confirmation->bindParam(":num_user", $num_user, PDO::PARAM_INT);
-$stmt_insert_confirmation->bindParam(":code", $code_confirmation);
-$stmt_insert_confirmation->execute();
+    // Capture de la sortie de la page PHP dans une variable
+    ob_start();
+    include("../mail/confirmation.php");
+    $content = ob_get_clean();
 
-//requête récuperer le mail de l'utilisateur
-$queryGetMail = "SELECT email FROM sae_users WHERE pseudo = :pseudo";
-$stmtGetmail = $cnx->prepare($queryGetMail);
-$stmtGetmail->bindParam(":pseudo", $pseudo, PDO::PARAM_STR);
-$stmtGetmail->execute();
-// Récupère le résultat
-$email = $stmtGetmail->fetchColumn();
+    $mail->addAddress($email, $pseudo);
 
-include("mailer.php");
-$mail->addAddress($email, $pseudo);
-
-// Contenu du mail
-$mail->isHTML(true);
-$mail->Subject = "Confirmation d'inscription";
-$mail->Body = "Bienvenue $pseudo sur notre site !<br><br>Veuillez confirmer votre inscription en cliquant sur le lien suivant : <a href='http://perso-etudiant.u-pem.fr/~chamsedine.amouche/Projet-SAE/inscription/script-confirmermail.php?code=$code_confirmation'>Confirmer</a>";
-$mail->CharSet = 'UTF-8';
-$mail->send();
+    // Contenu du mail
+    $mail->isHTML(true);
+    $mail->Subject = "Confirmation d'inscription";
+    $mail->Body = $content;
+    $mail->CharSet = "UTF-8";
+    $mail->AddEmbeddedImage("../img/monkey.png", "mylogo", "monkey.png", "base64", "image/png");
+    $mail->send();
 ?>
