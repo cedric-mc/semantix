42a1da1f784e5d15b0c1104b858e9ab159fb673b
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-03-14 12:01:45 +0100

    Modifications du script de photo de profil

--- profil/script-photo.php
+++ profil/script-photo.php
@@ -1,32 +1,28 @@
 <?php
-include_once("../class/User.php");
-include_once("../includes/conf.php");
-session_start();
-// Erreur PHP
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-if (!isset($_SESSION["user"])) {
-    header("Location: ./");
-    exit();
-}
-$user = User::createUserFromUser(unserialize($_SESSION["user"]));
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_FILES["photo"])) {
-    $image_data = file_get_contents($_FILES["photo"]["tmp_name"]);
-
-    $stmt = $cnx->prepare("UPDATE sae_users SET photo = ? WHERE num_user = ?");
-    $stmt->execute(array($image_data, $user->getIdUser()));
-    // Vérifier les erreurs de la requête
-    $errorInfo = $stmt->errorInfo();
-    if ($errorInfo[0] !== '00000') {
-        throw new Exception("Erreur lors de l'exécution de la requête : " . $errorInfo[2]);
+    include_once("../class/User.php");
+    include_once("../includes/conf.php");
+    session_start();
+    if (!isset($_SESSION["user"])) {
+        header("Location: ./");
+        exit();
     }
-    $user->setPhoto($image_data);
-    $_SESSION["user"] = serialize($user);
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_FILES["photo"])) {
+        $user = User::createUserFromUser(unserialize($_SESSION["user"]));
+        $image_data = file_get_contents($_FILES["photo"]["tmp_name"]);
+
+        $stmt = $cnx->prepare("UPDATE sae_users SET photo = ? WHERE num_user = ?");
+        $stmt->execute(array($image_data, $user->getIdUser()));
+        // Vérifier les erreurs de la requête
+        $errorInfo = $stmt->errorInfo();
+        if ($errorInfo[0] !== '00000') {
+            throw new Exception("Erreur lors de l'exécution de la requête : " . $errorInfo[2]);
+        }
+        $user->setImageData($image_data);
+        $_SESSION["user"] = serialize($user);
+        echo "<script>alert('Photo modifiée avec succès');</script>";
+        echo "<script>window.location.replace('./');</script>";
+        exit();
+    }
     header("Location: ./");
     exit();
-} else {
-    header("Location: ./");
-    exit();
-}
 ?>
