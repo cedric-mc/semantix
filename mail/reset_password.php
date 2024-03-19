01d19f1ac8d2466cb31f6058f1a5d5e0c62d782c
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-03-19 20:57:35 +0100

    Ajout du fichier reset_password.php pour les e-mails de réinitialisation de mot de passe

---
+++ mail/reset_password.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>Mail après réinitialisation de mot de passe</title>
+        <style>
+            header {
+                background-color: #f3f3f3;
+                padding: 20px;
+                text-align: center;
+            }
+
+            footer {
+                background-color: #f3f3f3;
+                padding: 20px;
+                text-align: center;
+            }
+
+            img {
+                width: 6.25rem;
+                height: auto;
+                margin: 20px;
+            }
+
+            p {
+                font-size: 20px;
+                text-align: center;
+            }
+        </style>
+    </head>
+
+    <body>
+        <header>
+            <h1>Réinitialisation de mot de passe</h1>
+            <img src="cid:mylogo" alt="Semantic Analogy Explorer" width="100px" height="100px">
+        </header>
+        <main>
+            <p>
+                Bonjour :pseudo,
+                <br><br>
+                Vous avez demandé la réinitialisation de votre mot de passe.
+                Si vous n'êtes pas à l'origine de la réinitialisation, veuillez changer immédiatement votre mot de passe ou contacter le support.
+                <br><br>
+                Cordialement,<br>
+                L'équipe de Semonkey.
+            </p>
+        </main>
+        <footer>
+            <p>© 2024 Semonkey. Tous droits réservés.</p>
+        </footer>
+    </body>
+</html>
