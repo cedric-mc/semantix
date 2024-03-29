84492ecee059c9e1b9fb457b59f86bba09625736
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-03-29 16:04:02 +0100

    Mise à jour de l'inclusion du fichier head.php dans confirm_change_password.php

--- profil/confirm_change_password.php
+++ profil/confirm_change_password.php
@@ -24,7 +24,7 @@
         <link rel="stylesheet" href="../style/form.css">
         <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-        <?php include("../includes/menu.php"); ?>
+        <?php include_once("../includes/head.php"); ?>
     </head>
 
     <body>
