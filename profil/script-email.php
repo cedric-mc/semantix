4b44229b556b724d86417be9a541db0a4d3b901d
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-03-29 14:50:13 +0100

    Mettre à jour la session utilisateur après modification de l'e-mail

--- profil/script-email.php
+++ profil/script-email.php
@@ -37,9 +37,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Modifier l'email
     $user->modifyEmail($cnx, $newEmail, $changeEmail);
-    // Création du nouveau User
-    $user = User::createUserFromUser($user);
-    $_SESSION['user'] = serialize($user);
+    $_SESSION['user'] = serialize($user); // Mettre à jour la session
 
     // Mail
     include("../mail/mailer.php");
