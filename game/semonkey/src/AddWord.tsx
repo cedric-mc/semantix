afcc596d525993c3dc006fe7208636558a90f346
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-03-29 17:06:26 +0100

    Ajouter Bootstrap et React Bootstrap pour le jeu du singe

--- game/semonkey/src/AddWord.tsx
+++ game/semonkey/src/AddWord.tsx
@@ -1,2 +1,22 @@
 import React from "react";
+// import FloatingLabel from "react-bootstrap/FloatingLabel";
+// import Form from "react-bootstrap/Form";
 
+const AddWord = () => {
+    return (
+        <div className="add-word">
+            <div className="form-floating mb-3">
+                <input type="text" className="form-control" id="word" name="word" placeholder="Nouveau mot" required />
+                <label form="word">Nouveau mot</label>
+            </div>
+            {/*<FloatingLabel controlId="word" label="Email address" className="form-control" />*/}
+            {/*    <Form.Control type="email" placeholder="name@example.com" />*/}
+            {/*</FloatingLabel>*/}
+            {/*<FloatingLabel controlId="floatingPassword" label="Password">*/}
+            {/*    <Form.Control type="password" placeholder="Password" />*/}
+            {/*</FloatingLabel>*/}
+        </div>
+    );
+}
+
+export default AddWord;
--- game/semonkey/src/App.tsx
+++ game/semonkey/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import AddWord from './AddWord';
 import './App.css';
 
 function App() {
     return (
         <div className="game-body">
-            
+            <header>
+                <h1>Le jeu du singe</h1>
+            </header>
+            <main>
+                <AddWord />
+            </main>
         </div>
     );
 }
