016c3f7ba8001fe5d05b009bd158e1780b88bfca
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-03-29 16:05:21 +0100

    Ajouter une inclusion de fichier fonctions.php dans le script-password.php

--- profil/script-password.php
+++ profil/script-password.php
@@ -10,6 +10,7 @@
         include_once("../includes/conf.php");
         include_once("../class/User.php");
         include_once("../includes/requetes.php");
+        include_once("../includes/fonctions.php");
         session_start();
         if (!isset($_SESSION['user'])) {
             header('Location: ../');
