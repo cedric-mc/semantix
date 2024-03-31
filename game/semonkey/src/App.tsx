2058ae81243d7a7726b5cfa55753a8e675c717c4
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-03-31 11:20:07 +0200

    Ajouter la classe User et le composant UserDisplay

--- game/semonkey/src/App.tsx
+++ game/semonkey/src/App.tsx
@@ -1,31 +1,41 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import AddWord from './AddWord';
 import "./style.css";
 import "./css_game.css";
 import ScoreBoard from "./ScoreBoard";
+import { User } from "./User";
+import UserDisplay from "./UserDisplay";
 import { Button } from 'react-bootstrap';
 
 function App() {
     const [word, setWord] = React.useState("");
     const [show, setShow] = React.useState(false);
+    const [user, setUser] = useState<User | null>(null);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
-    fetch("https://perso-etudiant.u-pem.fr/~mariyaconsta02/semantix/")
-        .then(response => response.json())
-        .then(data => {
-            console.log(data.field1);
-            console.log(data.field2);
-        });
+    useEffect(() => {
+        fetch('../')
+            .then(response => response.json())
+            .then(data => {
+                const user = new User(data.idUser, data.pseudo, data.email, data.year, data.imageData);
+                setUser(user);
+            });
+    }, []);
+
+    if (!user) {
+        return <div>Loading...</div>;
+    }
 
     return (
         <div className="game-body">
             <AddWord/>
             <Button variant="primary" onClick={handleShow}>Afficher les informations</Button>
-            <ScoreBoard show={show} handleClose={handleClose} />
+            <ScoreBoard show={show} handleClose={handleClose}/>
             <br></br>
             <Button className="btn btn-danger">Fin de partie</Button>
+            <UserDisplay user={user}/>
         </div>
     );
 }
