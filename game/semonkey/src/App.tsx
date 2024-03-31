22e8f243c7c85d900c037b0d0a39da1408267450
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-03-31 21:50:52 +0200

    Mise à jour du manifeste et du fichier de configuration TypeScript

--- game/semonkey/src/App.tsx
+++ game/semonkey/src/App.tsx
@@ -15,28 +15,6 @@ function App() {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
-    useEffect(() => {
-        // Accédez aux données `userData` qui ont été incluses dans votre page HTML
-        // @ts-ignore
-        const userData = window.userData;
-
-        // Créez une instance de l'objet User à partir des données userData
-        const userJson = new User(
-            userData.idUser,
-            userData.pseudo,
-            userData.email,
-            parseInt(userData.year), // Assurez-vous de convertir l'année en nombre si nécessaire
-            userData.imageData
-        );
-
-        // Définissez l'utilisateur dans l'état
-        setUser(userJson);
-    }, []);
-
-    if (!user) {
-        return <div>Loading...</div>;
-    }
-
     return (
         <div className="game-body">
             <AddWord/>
@@ -44,7 +22,7 @@ function App() {
             <ScoreBoard show={show} handleClose={handleClose}/>
             <br></br>
             <Button className="btn btn-danger">Fin de partie</Button>
-            <UserDisplay user={user}/>
+            {/*<UserDisplay user={user}/>*/}
         </div>
     );
 }
