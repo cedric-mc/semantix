e0cc581c2fc879af4a8e479219c7d5339228eb0a
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-04-01 19:50:33 +0200

    Mise à jour des balises méta dans head.php

--- includes/head.php
+++ includes/head.php
@@ -1,7 +1,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description" content="Jeu en ligne à un ou plusieurs joueurs basé sur les similarités entre mots : « Semonkey ». Chaque joueur reçoit un mot de départ et un mot cible et propose des mots proches afin de créer une chaîne de mots similaires pour relier le mot de départ au mot cible.">
-mots : « Semantic Analogy Explorer ».">
-<meta name="keywords" content="">
+<meta name="keywords" content="Semonkey, jeu, en ligne, multijoueur, mots, similarités, chaîne, mots, mot, départ, mot, cible, proches, relier">
 <meta name="author" content="Mamadou BA, Cédric MARIYA CONSTANTINE, Abdelrahim RICHE, Vincent SOUSA, Yacine ZEMOUCHE">
 <script src="https://kit.fontawesome.com/3f3ecfc27b.js"></script>
 <title>Semonkey</title>
