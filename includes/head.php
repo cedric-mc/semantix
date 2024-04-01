9efd6d0b2087282fb82f27379797819b7f561109
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-04-01 19:47:07 +0200

    Mise à jour de la balise meta description dans head.php

--- includes/head.php
+++ includes/head.php
@@ -1,5 +1,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="description" content="">
+<meta name="description" content="Jeu en ligne à un ou plusieurs joueurs basé sur les similarités entre mots : « Semonkey ». Chaque joueur reçoit un mot de départ et un mot cible et propose des mots proches afin de créer une chaîne de mots similaires pour relier le mot de départ au mot cible.">
+mots : « Semantic Analogy Explorer ».">
 <meta name="keywords" content="">
 <meta name="author" content="Mamadou BA, Cédric MARIYA CONSTANTINE, Abdelrahim RICHE, Vincent SOUSA, Yacine ZEMOUCHE">
 <script src="https://kit.fontawesome.com/3f3ecfc27b.js"></script>
