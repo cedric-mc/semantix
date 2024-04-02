b59e1b3ed6fb2b2a1b34c25a4c5335eb62d6f3e3
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-04-02 10:19:34 +0200

    Mise à jour de la sérialisation des données utilisateur dans index.php

--- index.php
+++ index.php
@@ -34,7 +34,13 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
         <script>
             // Encodé en json les données de l'utilisateur
-            let user = <?php echo json_encode($user); ?>;
+            let user = {
+                idUser: <?php echo $user->getIdUser(); ?>,
+                pseudo: "<?php echo $user->getPseudo(); ?>",
+                email: "<?php echo $user->getEmail(); ?>",
+                year: <?php echo $user->getYear(); ?>,
+                imageData: "<?php echo $user->getImageData(); ?>"
+            };
             // Stocker les données de l'utilisateur dans le localStorage
             localStorage.setItem('user', JSON.stringify(user));
 
