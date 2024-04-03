8d23e476474463598f9d11bc967e68651a1cda9b
cedric-mc <106695106+cedric-mc@users.noreply.github.com>
2024-04-03 14:50:11 +0200

    Changement de paramètre de recherche dans LoginActivity.java

--- app_mobile/app/src/main/java/com/but/semonkey/LoginActivity.java
+++ app_mobile/app/src/main/java/com/but/semonkey/LoginActivity.java
@@ -115,7 +115,7 @@ public class LoginActivity extends AppCompatActivity {
                     connection.setDoOutput(true);
 
                     // Construire les données à envoyer
-                    String postData = "search=" + URLEncoder.encode(username, "UTF-8") +
+                    String postData = "pseudo=" + URLEncoder.encode(username, "UTF-8") +
                             "&motdepasse=" + URLEncoder.encode(password, "UTF-8");
 
                     // Envoyer les données d'authentification
