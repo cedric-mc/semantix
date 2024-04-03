d46ff8d2f743040e5751620995c19f9bf4f97039
cedric-mc <cedric.mc11@gmail.com>
2024-04-03 10:18:43 +0200

    03/04 10:18

---
+++ semonkey/index.php
@@ -0,0 +1,36 @@
+<?php
+    include_once("../class/User.php");
+    session_start();
+    if (!isset($_SESSION['user'])) {
+        header('Location: ../');
+        exit;
+    }
+    $user = User::createUserFromUser(unserialize($_SESSION['user']));
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width,initial-scale=1"/>
+    <title>Semonkey</title>
+    <script defer="defer" src="static/js/main.18ed5e47.js"></script>
+    <link rel="shortcut icon" href="../img/monkeyapp.png">
+    <link href="static/css/main.d81cbcc2.css" rel="stylesheet">
+    <link rel="stylesheet" href="../../../style/style.css">
+    <?php include("../includes/header.php"); ?>
+</head>
+<body>
+<noscript>You need to enable JavaScript to run this app.</noscript>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.1.0/react.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.1.0/react-dom.min.js"></script>
+<div id="root"></div>
+<script>
+    let userData = {
+        idUser: <?php echo $user->getIdUser(); ?>,
+        pseudo: "<?php echo $user->getPseudo(); ?>",
+    };
+    console.log(userData);
+    localStorage.setItem("userData", JSON.stringify(userData))
+</script>
+</body>
+</html>
